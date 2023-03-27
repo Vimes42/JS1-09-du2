8c4288a04ca50c37c2af4261098e99adf056c235
Lenka Polcerova <lenka.polcerova@gmail.com>
2023-03-27 22:25:50 +0200

    hotovo - i s bodem d

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ const user = {
 const registrace = document.querySelector("#registration")
 const emailInput = document.querySelector(".emailInput")
 const passwordInput = document.querySelector(".passwordInput")
+const nadpis = document.querySelector(".nadpis")
 
 const overit = (event) => {
     event.preventDefault()
     if (passwordInput.value === user.password && emailInput.value === user.email) {
-        registration.textContent = "Přihlášený uživatel: Bartoloměj"
+        registration.textContent = `Přihlášený uživatel: ${user.name}`
     } else {
-        registration.textContent = "Špatné jméno nebo heslo."
+        nadpis.textContent = "Špatné jméno nebo heslo."
+        nadpis.style.color = "red"
+        passwordInput.value = ""
     }
 }
 
